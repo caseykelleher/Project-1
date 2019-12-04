40ec3f2769ee59359a7a3b9ead5efa3fdd06c347
Casey K <caseykelleher@icloud.com>
2019-12-03 21:24:55 -0500

    fix

--- assets/java/meme.js
+++ assets/java/meme.js
@@ -7,7 +7,7 @@ var emotion = [];
 var place = [];
 
 function findLocation () {
-    $.ajax('http://ip-api.com/json')
+    $.ajax('https://ip-api.com/json')
     .then(
         function success(response) {
             console.log("city", response.city)
